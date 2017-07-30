dc0d1802938451e691c69e827309f065009e80e8
ImmersiveS <seruy5432@gmail.com>
2017-07-30 22:43:25 +0300

    Delete unnecessary files; add settings and bot classes

---
+++ SimpleBot/Models/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://telegrambotapp.azurewebsites.net:443/{0}";
+
+        public static string Name { get; set; } = "John_Clucker_bot";
+
+        public static string Key { get; set; } = "403416362:AAHmgIGqzQTQX1nN7QJSurQgOpWGKxzxW1k";
+    }
+}
